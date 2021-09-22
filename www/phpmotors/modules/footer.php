f7192af7f2b04955feab87b869ae9421ae7fe46e
dirogers <rog17006@byui.edu>
2021-09-21 17:44:46 -0700

    Created new folder for Github upload.

---
+++ www/phpmotors/modules/footer.php
@@ -0,0 +1,5 @@
+<div class="footer">
+    <p class="copyright">&copy; PHP Motors, All rights reserved</p>
+    <p class="copyright">All images used are believed to be in "Fair Use". Please notify the author if any are not and they will be removed.</p>
+    <p class="copyright">Last Updated: 30 March, 2018</p>
+</div>
---
+++ www/phpmotors/modules/header.php
@@ -0,0 +1,4 @@
+<div class="logo-bar">
+    <img src="./images/site/logo.png" alt="Logo for PHPMotors">
+    <p> My Account</p>
+</div>
